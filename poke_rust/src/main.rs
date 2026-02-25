5dabc8e902b989f540f44b6dc75581aee52a1581
nlaud <nathan.laud@gmail.com>
2026-02-24 20:39:40 -0500

    CLI Parsing, Showdown Data
    Added CLI Parsing with clap library, and imported some .txt files with data about moves and pokemon from showdown repo

---
+++ poke_rust/src/main.rs
@@ -0,0 +1,29 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author = "Blazestorm", version = "1.0", about = "Simulates Pokemon Battles")]
+struct Args {
+    /// Path to the first player's teamsheet
+    #[arg(long)]
+    p1: String,
+
+    /// Path to the second player's teamsheet
+    #[arg(long)]
+    p2: String,
+
+    /// How verbose debug output is (0 = Nothing, 1 = Minimal, 2 = Debug Trace)
+    #[arg(short, long, default_value_t = 1)]
+    verbosity: u8,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    if args.verbosity >= 2 { println!("Got paths: {}, {}", args.p1, args.p2) }
+
+    //Put dex data into a hashmap
+
+    //Put move data into a hashmap
+
+    //Parse teamsheets into teams
+}
